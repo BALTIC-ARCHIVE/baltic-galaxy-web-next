ef2736dd40010f0fefa12c8904c2295c07ad175b
John Delenschke <johndelenschke@outlook.de>
2024-08-15 21:45:59 +0200

    fix links

--- app/layout.tsx
+++ app/layout.tsx
@@ -107,12 +107,12 @@ export default function RootLayout({children,}: {
                         <div>
                             <ul className="mt-2 list-disc">
                                 <li className="inline mx-5">
-                                    <a href="https://www.baltic-studios.net/de/privacy" className="hover:text-gray-400 text-gray-500 hover:cursor-pointer">
+                                    <a href="https://www.baltic-studios.net/privacy" className="hover:text-gray-400 text-gray-500 hover:cursor-pointer">
                                         Datenschutz
                                     </a>
                                 </li>
                                 <li className="inline mx-5">
-                                    <a href="https://www.baltic-studios.net/de/imprint" className="hover:text-gray-400 text-gray-500 hover:cursor-pointer">
+                                    <a href="https://www.baltic-studios.net/imprint" className="hover:text-gray-400 text-gray-500 hover:cursor-pointer">
                                         Impressum
                                     </a>
                                 </li>
